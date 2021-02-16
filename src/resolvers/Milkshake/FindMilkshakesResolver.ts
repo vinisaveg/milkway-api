1c89d787e7679ffeb282cb9fb05ac64e63f03bb9
vinisaveg <viniisaveeg@gmail.com>
2021-02-16 08:43:07 -0300

    Limit range on FindMilkshakesResolver :wrench:

--- src/resolvers/Milkshake/FindMilkshakesResolver.ts
+++ src/resolvers/Milkshake/FindMilkshakesResolver.ts
@@ -1,4 +1,4 @@
-import { Ctx, Query, Resolver } from 'type-graphql';
+import { Arg, Ctx, Query, Resolver } from 'type-graphql';
 
 import { Milkshake } from '@entities/milkshake/Milkshake';
 import { Context } from '@context/createContext';
@@ -6,8 +6,13 @@ import { Context } from '@context/createContext';
 @Resolver()
 export class FindMilkshakesResolver {
     @Query((returns) => [Milkshake])
-    async findMilkshakes(@Ctx() ctx: Context): Promise<Array<Milkshake>> {
-        const milkshakes = await ctx.prisma.milkshake.findMany();
+    async findMilkshakes(
+        @Ctx() ctx: Context,
+        @Arg('limit', { nullable: true }) limit: number
+    ): Promise<Array<Milkshake>> {
+        const milkshakes = await ctx.prisma.milkshake.findMany({
+            take: limit,
+        });
 
         return milkshakes;
     }
