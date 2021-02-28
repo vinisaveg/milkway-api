05d8cbb9530b70fd3f7d759e98f1896cc65366c6
vinisaveg <viniisaveeg@gmail.com>
2021-02-28 09:26:36 -0300

    FindUserMilkshakes adjusted :wrench: :zap:

--- src/resolvers/Milkshake/FindUserMilkshakesResolver.ts
+++ src/resolvers/Milkshake/FindUserMilkshakesResolver.ts
@@ -8,16 +8,21 @@ export class FindUserMilkshakesResolver {
     @Query((returns) => [Milkshake])
     async findUserMilkshakes(
         @Ctx() ctx: Context,
-        @Arg('id') userId: number,
         @Arg('limit', { nullable: true }) limit: number
     ): Promise<Array<Milkshake>> {
-        const userMilkshakes = await ctx.prisma.milkshake.findMany({
-            where: {
-                userId: userId,
-            },
-            take: limit,
-        });
+        let userId = await (ctx.session as any).userId;
 
-        return userMilkshakes;
+        if (userId) {
+            const userMilkshakes = await ctx.prisma.milkshake.findMany({
+                where: {
+                    userId: userId,
+                },
+                take: limit,
+            });
+
+            return userMilkshakes;
+        }
+
+        return [];
     }
 }
